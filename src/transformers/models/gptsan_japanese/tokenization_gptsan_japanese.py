8cfa5bc216099b708677abbc6e0353eaa265a522
Ita Zaporozhets <itazaporozhets@Itas-MBP.localdomain>
2024-05-14 16:44:43 +0200

    passing rest of args to gptsan_japanese, fixing tests

--- src/transformers/models/gptsan_japanese/tokenization_gptsan_japanese.py
+++ src/transformers/models/gptsan_japanese/tokenization_gptsan_japanese.py
@@ -352,6 +352,7 @@ class GPTSanJapaneseTokenizer(PreTrainedTokenizer):
         return_offsets_mapping: bool = False,
         return_length: bool = False,
         verbose: bool = True,
+        **kwargs,
     ) -> BatchEncoding:
         # This tokenizer converts input text pairs into Prefix input and subsequent input
         if isinstance(batch_text_or_text_pairs[0], tuple) or isinstance(tuple(batch_text_or_text_pairs[0]), list):
@@ -378,6 +379,7 @@ class GPTSanJapaneseTokenizer(PreTrainedTokenizer):
             return_offsets_mapping,
             return_length,
             verbose,
+            **kwargs,
         )
 
 
