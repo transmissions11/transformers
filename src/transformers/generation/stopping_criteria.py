b0ce9bc873f0ce0b730a4c89a78868e23a3a3ebc
ydshieh <ydshieh@users.noreply.github.com>
2024-05-21 14:25:44 +0200

    fix

--- src/transformers/generation/stopping_criteria.py
+++ src/transformers/generation/stopping_criteria.py
@@ -481,6 +481,7 @@ class EosTokenCriteria(StoppingCriteria):
 
     @add_start_docstrings(STOPPING_CRITERIA_INPUTS_DOCSTRING)
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> torch.BoolTensor:
+        self.eos_token_id = self.eos_token_id.to(input_ids.device)
         if input_ids.device.type == "mps":
             # https://github.com/pytorch/pytorch/issues/77764#issuecomment-2067838075
             is_done = (
